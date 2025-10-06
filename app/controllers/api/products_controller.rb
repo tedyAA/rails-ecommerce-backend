45b8f0df5c45ec53e20c002e89c4ac55d90bba18
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2025-10-06 13:31:44 +0300

    added sort

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,42 +3,51 @@ module Api
     include Rails.application.routes.url_helpers
 
     def index
-  products = Product.includes(:category, :type, images_attachments: :blob)
+      products = Product.includes(:category, :type, images_attachments: :blob)
 
-  products = products.where(bestseller: params[:bestseller] == "true") if params[:bestseller].present?
-  products = products.where(category_id: params[:category].split(',').map(&:to_i)) if params[:category].present?
-  products = products.where(type_id: params[:type].split(',').map(&:to_i)) if params[:type].present?
+      products = products.where(bestseller: params[:bestseller] == "true") if params[:bestseller].present?
+      products = products.where(category_id: params[:category].split(',').map(&:to_i)) if params[:category].present?
+      products = products.where(type_id: params[:type].split(',').map(&:to_i)) if params[:type].present?
 
-  if params[:term].present?
-    term = params[:term].downcase
-    products = products.where("LOWER(name) LIKE ?", "%#{term}%")
-  end
+      if params[:term].present?
+        term = params[:term].downcase
+        products = products.where("LOWER(name) LIKE ?", "%#{term}%")
+      end
 
-  products = products.order(Arel.sql('RANDOM()')) if params[:random] == "true"
+      products = products.order(Arel.sql('RANDOM()')) if params[:random] == "true"
 
-  # Manual pagination
-  page = (params[:page] || 1).to_i
-  per_page = (params[:per] || 12).to_i
-  offset = (page - 1) * per_page
+      if params[:sort].present?
+        case params[:sort]
+        when "high-low"
+          products = products.order(price: :desc)
+        when "low-high"
+          products = products.order(price: :asc)
+        else
+        
+        end
+      end
 
-  total_count = products.count
-  products = products.offset(offset).limit(per_page)
+      page = (params[:page] || 1).to_i
+      per_page = (params[:per] || 12).to_i
+      offset = (page - 1) * per_page
 
-  render json: {
-    current_page: page,
-    total_pages: (total_count.to_f / per_page).ceil,
-    total_count: total_count,
-    products: products.map do |product|
-      {
-        **product.attributes.symbolize_keys,
-        category: product.category,
-        type: product.type,
-        image_urls: product.images.map { |img| url_for(img) }
+      total_count = products.count
+      products = products.offset(offset).limit(per_page)
+
+      render json: {
+        current_page: page,
+        total_pages: (total_count.to_f / per_page).ceil,
+        total_count: total_count,
+        products: products.map do |product|
+          {
+            **product.attributes.symbolize_keys,
+            category: product.category,
+            type: product.type,
+            image_urls: product.images.map { |img| url_for(img) }
+          }
+        end
       }
     end
-  }
-end
-
 
     def show
       product = Product.includes(:category, images_attachments: :blob).find_by(id: params[:id])
